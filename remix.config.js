52e7eb7557ebc1039964c071a3c7d3bd9ffc5331
joshua-myers <josh.myers@headway.io>
2022-05-19 17:35:58 +0000

    use vercel server

--- remix.config.js
+++ remix.config.js
@@ -4,5 +4,21 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverBuildTarget: "vercel"
+  serverBuildTarget: "vercel",
+  /**
+   * Remove if not using Vercel
+   *
+   * Inspiredf from DNB stack.
+   *
+   * @example
+   * yarn run dev => remix dev server
+   * yarn run build => use remix prod server, locally
+   * VERCEL=1 yarn run build => use vercel server, locally
+   * On Vercel: => yarn run build will use vercel server (VERCEL is automatically set)
+   *
+   * @see https://vercel.com/docs/concepts/projects/environment-variables
+   *
+   * @see https://github.com/robipop22/dnb-stack/blob/main/remix.config.js
+   */
+  server: process.env.VERCEL ? "./vercel.server.js" : undefined,
 };
